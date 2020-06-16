98a9d1deee6aaeb7dca48dee3554a06a8d2dc327
pmaldagu <pierremaldague@gmail.com>
2020-06-16 16:28:03 +0200

    write

---
+++ in_progress/ft_write.s
@@ -0,0 +1,6 @@
+	global _ft_write
+
+_ft_write:
+	mov	rax, 0x2000004
+	syscall
+	ret
--- in_progress/libasm.h
+++ in_progress/libasm.h
@@ -10,5 +10,6 @@ size_t	ft_strlen(const char *str);
 int	ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
+ssize_t	ft_write(int fd, void const *buf, size_t nbyte);
 
 #endif
