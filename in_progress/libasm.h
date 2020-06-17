7c84e5c09ea3d4d54b32da0de89433df0cce7428
pmaldagu <pierremaldague@gmail.com>
2020-06-17 12:06:59 +0200

    waiting for correction

--- in_progress/libasm.h
+++ in_progress/libasm.h
@@ -11,5 +11,6 @@ int	ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 ssize_t	ft_write(int fd, void const *buf, size_t nbyte);
+ssize_t	ft_read(int fd, void *buf, size_t nbyte);
 
 #endif
--- in_progress/main.c
+++ in_progress/main.c
@@ -6,7 +6,7 @@
 /*   By: pmaldagu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/15 12:02:10 by pmaldagu          #+#    #+#             */
-/*   Updated: 2020/06/17 11:45:00 by pmaldagu         ###   ########.fr       */
+/*   Updated: 2020/06/17 12:04:31 by pmaldagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,19 @@ int main()
 	printf("%zu\n", ft_write(4, "aabxbc", 1));
 	printf("%zu\n", write(4, "aabxbac", 1));
 	printf("===========END==========\n");
+
+	int    fd;
+	char    buffer[40];
+	int    i = 0;
+
+	while(i < 40)
+	{
+		buffer[i] = 0;
+		i++;
+	}
+	fd = open("./test.txt",    O_RDWR);
+	printf("%zd\n",ft_read(fd, buffer, 39));
+	printf("buffer||%s\n", buffer);
+
 	return (0);
 }
